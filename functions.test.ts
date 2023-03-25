bd67550769d187b5afde3401257b1375eda54c48
Anna Brooksby <johnhbrooksby@yahoo.com>
2023-03-25 00:40:14 -0600

    Functions.test.ts complete

--- functions.test.ts
+++ functions.test.ts
@@ -2,4 +2,10 @@ const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
     // CODE HERE
+    test('be a valid function', () => {
+        expect(typeof (shuffleArray)).toBe("function")
+    })
+    test('be greater than 4', () => {
+        expect(shuffleArray([1, 2, 3, 4, 5]).length).toBeGreaterThan(4)
+    })
 })
